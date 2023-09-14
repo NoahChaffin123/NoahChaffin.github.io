245a6d70b38e72f85742b253e3828ca77b5b776e
Noah Chaffin <ChaffinNC@hendrix.edu>
2023-09-14 14:49:53 -0500

    Checking to see if my webpage works

---
+++ csci340lab4/script.js
@@ -0,0 +1,34 @@
+const api_url= "https://catfact.ninja/fact"; 
+
+
+$(document).ready(function() {
+  $('#clicker').click(function() {
+    console.log("Testing")
+    $.ajax({
+      dataType: "json",
+      url: "https://catfact.ninja/fact",
+      success: function(results) {
+        console.log(results);
+        $("#cat-fact").html(results['fact']);
+      },
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
+    });
+  }); 
+
+  $('#clicker-two').click(function() {
+    $.ajax({
+      dataType: "json",
+      url: "https://randomfox.ca/floof/",
+      success: function(statement) {
+        console.log(statement["id"]);
+        $('#kittens').attr("src", statement["image"]);
+        
+      },
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
+    });
+  });
+});
